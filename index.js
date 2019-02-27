820335f382cdf089c7c76718be5f72ac6a37c998
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-27 01:03:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ document.getElementById("username").addEventListener('change', function(e) {
   getRepositories(e) ;
 })
 function getRepositories(e) {
-  console.log(e.target.value) ; 
+  console.log(e.target.value) ;
   const repoPath = "https://api.github.com/users/" + e.target.value + "/repos"
   const req = new XMLHttpRequest();
   //add the callback trigger here
-  req.open("GET",repoPath) ; 
+  req.open("GET",repoPath) ;
 }
