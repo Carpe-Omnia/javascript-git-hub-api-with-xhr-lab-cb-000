d0313e613afbb7285e9146036af0e2694fa37734
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-27 02:19:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function displayCommits() {
     .map(
       commit =>
         "<li>" +
-        commit.committer.login + " - " +
+        commit.commit.committer.login + " - " +
         commit.commit.author.name +
         " - " +
         commit.commit.message +
