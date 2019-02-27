6a8816b89d059b2ef5537243d1db09c6d36ff179
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-27 00:59:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ document.getElementById("username").addEventListener('change', function(e) {
   getRepositories(e) ;
 })
 function getRepositories(e) {
-  console.log(e.value)
+  console.log(e.target.value)
 }
